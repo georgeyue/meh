7905606ce597fb5389b80a4ff446fdb58e9264a3
George Yue <george.yue@ticketmaster.com>
2016-04-29 16:56:56 -0700

    add react, enzyme to tests

---
+++ lib/components/meh.jsx
@@ -0,0 +1,11 @@
+'use strict';
+import React from 'react';
+import {shallow} from 'enzyme';
+
+module.exports = React.createClass({
+  displayName: 'Meh Component',
+
+  render: () => {
+    return (<div>meh</div>);
+  }
+});
--- test/meh.spec.js
+++ test/meh.spec.js
@@ -1,10 +1,21 @@
+import React from 'react';
 import {expect} from 'chai';
+import {shallow} from 'enzyme';
+
+import Meh from '../lib';
 
 describe('Array', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       expect([1,2,3].indexOf(5)).to.equal(-1);
       expect([1,2,3].indexOf(0)).to.equal(-1);
+    });
+  });
+
+  describe("Meh", function() {
+    it("init", function() {
+      var wrapper = shallow(<Meh />);
+      expect(wrapper.text()).to.equal('meh');
     });
   });
 });
